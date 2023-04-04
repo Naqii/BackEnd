dbdb6e5d11956c08356c19de7fa5eab672774e86
Naqi <sunanhaqi.sh@gmail.com>
2023-04-04 09:47:12 +0700

    second commit

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const http = require('http');
 
 const requestListener = (request, response) => {
-    response.setHeader('Content-Type', 'text/html');
-    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+    response.setHeader('X-Powerd-By', 'NodeJS');
+    
     const { method, url } = request;
 
     if (url === '/') {
         if (method === 'GET') {
+            response.statusCode = 200;
             response.end('<h1>Ini adalah Homepage!</h1>');
         } else {
+            response.statusCode = 400;
             response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
         }
     } else if (url === '/about') {
         if (method === 'GET') {
-            response.end('<h1>Ini adalah About!</h1>');
+            response.statusCode = 200;
+            response.end('<h1>Hallo, Ini adalah About!</h1>');
         } else if (method === 'POST') {
             let body = [];
 
@@ -27,9 +31,11 @@ const requestListener = (request, response) => {
                 response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
             })
         } else {
+            response.statusCode = 400;
             response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
         }
     } else {
+        response.statusCode = 404;
         response.end('<h1>Halaman tidak ditemukan!</h1>');
     }
 };
